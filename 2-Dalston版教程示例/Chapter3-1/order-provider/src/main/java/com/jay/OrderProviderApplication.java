8b9ab6589fd447fe5b6885854b4c4e54464dee55
XWxiaowei <2809641033@qq.com>
2019-01-21 21:10:08 +0800

    处理

--- 2-Dalston版教程示例/Chapter3-1/order-provider/src/main/java/com/jay/OrderProviderApplication.java
+++ 2-Dalston版教程示例/Chapter3-1/order-provider/src/main/java/com/jay/OrderProviderApplication.java
@@ -2,6 +2,8 @@ package com.jay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
